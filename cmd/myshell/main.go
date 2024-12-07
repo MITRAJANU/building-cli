48b01aeb5b08b5b0835963ccb3de0af7b549cb89
MITRAJANU <mitrajanu.mishra@gmail.com>
2024-12-07 11:13:37 +0530

    ADD:REPL

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -14,12 +14,12 @@ var _ = fmt.Fprint
 func main() {
 	for {
 		// Prompt for user input
-		fmt.Fprint(os.Stdout, "$ ")
+		fmt.Print("$ ")
 
 		// Read user input
 		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
-			fmt.Println("Error reading input:", err)
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			continue
 		}
 
@@ -34,7 +34,7 @@ func main() {
 		// Split the command into name and arguments
 		args := strings.Fields(command)
 		cmdName := args[0]
-		
+
 		// Execute the command and handle errors
 		if err := executeCommand(cmdName, args[1:]); err != nil {
 			fmt.Printf("%s: %s\n", cmdName, err.Error())
