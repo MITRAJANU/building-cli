189951372faf7591d21cb790b60a25d380b9ebe5
MITRAJANU <mitrajanu.mishra@gmail.com>
2024-12-07 13:05:30 +0530

    FIX:Backslash outside quotes

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -1,268 +1,245 @@
 package main
 
 import (
+
 	"bufio"
+
 	"fmt"
+
+	"log"
+
 	"os"
+
 	"os/exec"
-	"path/filepath"
+
+	"path"
+
+	"regexp"
+
+	"strconv"
+
 	"strings"
+
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
-
-// List of built-in commands
-var builtins = map[string]string{
-	"echo": "is a shell builtin",
-	"exit": "is a shell builtin",
-	"type": "is a shell builtin",
-	"pwd":  "is a shell builtin",
-	"cd":   "is a shell builtin",
-}
-
 func main() {
+
 	for {
-		// Prompt for user input
-		fmt.Print("$ ")
 
-		// Read user input
-		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		// Uncomment this block to pass the first stage
+
+		fmt.Fprint(os.Stdout, "$ ")
+
+		// Wait for user input
+
+		s, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading input:", err)
-			continue
+
+			log.Fatal(err)
+
 		}
 
-		// Trim whitespace and newlines from input
-		command := strings.TrimSpace(input)
+		s = strings.Trim(s, "\r\n")
 
-		// Check if the command is empty
-		if command == "" {
-			continue // Ignore empty commands
-		}
+		var args []string
 
-		// Split the command into name and arguments, handling quotes
-		cmdName, args := parseCommand(command)
+		command, argstr, _ := strings.Cut(s, " ")
 
-		if cmdName == "exit" {
-			exitCode := 0 // Default exit code
-			if len(args) > 0 {
-				fmt.Sscanf(args[0], "%d", &exitCode) // Get exit code if provided
+		if strings.Contains(s, "\"") {
+
+			re := regexp.MustCompile("\"(.*?)\"")
+
+			args = re.FindAllString(s, -1)
+
+			for i := range args {
+
+				args[i] = strings.Trim(args[i], "\"")
+
 			}
-			os.Exit(exitCode) // Exit with the specified code
-		}
 
-        if cmdName == "echo" {
-            handleEcho(args) // Handle echo command
-            continue
-        }
+		} else if strings.Contains(s, "'") {
 
-        if cmdName == "pwd" {
-            handlePwd() // Handle pwd command
-            continue
-        }
+			re := regexp.MustCompile("'(.*?)'")
 
-        if cmdName == "cd" {
-            handleCd(args) // Handle cd command
-            continue
-        }
+			args = re.FindAllString(s, -1)
 
-        if cmdName == "type" {
-            handleType(args) // Handle type command
-            continue
-        }
+			for i := range args {
 
-        // Execute external commands and handle errors.
-        if err := executeExternalCommand(cmdName, args); err != nil {
-            fmt.Printf("%s: %s\n", cmdName, err.Error())
-        }
-    }
-}
+				args[i] = strings.Trim(args[i], "'")
 
-// parseCommand splits the command string into the command name and arguments,
-// handling both single and double quotes for literal values.
-func parseCommand(input string) (string, []string) {
-	var cmdName string
-	var args []string
-	var currentArg strings.Builder
-	inSingleQuote := false
-	inDoubleQuote := false
-	escaped := false
-
-	for _, char := range input {
-		switch char {
-		case '\\':
-			if escaped {
-				currentArg.WriteRune(char) // Add literal backslash
-				escaped = false
-			} else {
-				escaped = true // Mark that the next character should be treated literally
 			}
-		case '\'':
-			if escaped {
-				currentArg.WriteRune(char) // Add literal single quote
-				escaped = false
-			} else if inDoubleQuote {
-				currentArg.WriteRune(char) // Add literal single quote inside double quotes
-			} else {
-				inSingleQuote = !inSingleQuote // Toggle single-quote state
-			}
-		case '"':
-			if escaped {
-				currentArg.WriteRune(char) // Add literal double quote
-				escaped = false
-			} else if inSingleQuote {
-				currentArg.WriteRune(char) // Add literal double quote inside single quotes
-			} else {
-				inDoubleQuote = !inDoubleQuote // Toggle double-quote state
-			}
-		case ' ':
-			if escaped || inSingleQuote || inDoubleQuote { // If inside quotes or escaped, treat space literally
-				currentArg.WriteRune(char)
-				escaped = false // Reset escape state after using it
-			} else {
-				if currentArg.Len() > 0 { // End of an argument
-					args = append(args, currentArg.String())
-					currentArg.Reset()
+
+		} else {
+
+			args = strings.Fields(argstr)
+
+			if strings.Contains(argstr, "\\") {
+
+				re := regexp.MustCompile(`[^\\] +`)
+
+				args = re.Split(argstr, -1)
+
+				for i := range args {
+
+					args[i] = strings.ReplaceAll(args[i], "\\", "")
+
 				}
+
+			} else {
+
+				args = strings.Fields(argstr)
+
 			}
+
+		}
+
+		switch command {
+
+		case "cd":
+
+			if args[0] == "~" {
+
+				args[0] = os.Getenv("HOME")
+
+			}
+
+			if err := os.Chdir(args[0]); os.IsNotExist(err) {
+
+				fmt.Println(command + ": " + args[0] + ": No such file or directory")
+
+				break
+
+			} else if err != nil {
+
+				log.Fatal(err)
+
+			}
+
+		case "echo":
+
+			fmt.Println(strings.Join(args, " "))
+
+		case "exit":
+
+			n, err := strconv.Atoi(args[0])
+
+			if err != nil {
+
+				log.Fatal(err)
+
+			}
+
+			os.Exit(n)
+
+		case "pwd":
+
+			dir, err := os.Getwd()
+
+			if err != nil {
+
+				log.Fatal(err)
+
+			}
+
+			fmt.Println(dir)
+
+		case "type":
+
+			var isBuiltin bool
+
+			var isExecutable bool
+
+			var cmdPath string
+
+			builtin := []string{"cd", "echo", "exit", "pwd", "type"}
+
+			for _, cmd := range builtin {
+
+				if args[0] == cmd {
+
+					isBuiltin = true
+
+					break
+
+				}
+
+			}
+
+			var separator string
+
+			pathVar := os.Getenv("PATH")
+
+			if strings.Contains(pathVar, ";") {
+
+				separator = ";"
+
+			} else {
+
+				separator = ":"
+
+			}
+
+			dirs := strings.Split(pathVar, separator)
+
+			for _, dir := range dirs {
+
+				_, err = os.Stat(path.Join(dir, args[0]))
+
+				if err == nil {
+
+					isExecutable = true
+
+					cmdPath = path.Join(dir, args[0])
+
+					break
+
+				}
+
+			}
+
+			if isBuiltin {
+
+				fmt.Println(args[0] + " is a shell builtin")
+
+			} else if isExecutable {
+
+				fmt.Println(args[0] + " is " + cmdPath)
+
+			} else {
+
+				fmt.Println(args[0] + ": not found")
+
+			}
+
 		default:
-			if escaped {
-				currentArg.WriteRune('\\') // Write the backslash literally before the current character.
+
+			_, err = exec.LookPath(command)
+
+			if err != nil {
+
+				fmt.Println(command + ": command not found")
+
+				break
+
 			}
-			currentArg.WriteRune(char) // Add regular character.
-			escaped = false // Reset escape state after using it.
-		}
-	}
 
-	if escaped { // Handle any leftover escape character at the end of input.
-		currentArg.WriteRune('\\')
-	}
+			cmd := exec.Command(command, args...)
 
-	if currentArg.Len() > 0 { // Add last argument if exists.
-		args = append(args, currentArg.String())
-	}
+			cmd.Stdin = os.Stdin
 
-	if len(args) > 0 {
-		cmdName = args[0]
-		args = args[1:] // Remove command name from arguments.
-	}
+			cmd.Stdout = os.Stdout
 
-	return cmdName, args
-}
+			cmd.Stderr = os.Stderr
 
+			if err := cmd.Run(); err != nil {
 
-// handleEcho prints the provided arguments as a single string.
-func handleEcho(args []string) {
-	var output strings.Builder
+				log.Fatal(err)
 
-	for i, arg := range args {
-		for j := 0; j < len(arg); j++ {
-			if arg[j] == '\\' {
-				// Write the backslash itself.
-				output.WriteByte(arg[j])
-				j++ // Move to the next character.
-				if j < len(arg) {
-					output.WriteByte(arg[j]) // Write the next character as well.
-				}
-			} else {
-				output.WriteByte(arg[j]) // Write the current character.
 			}
+
 		}
-		if i < len(args)-1 {
-			output.WriteByte(' ') // Add a space between arguments.
-		}
+
 	}
 
-	fmt.Println(output.String())
-}
-// handlePwd prints the current working directory.
-func handlePwd() {
-	dir, err := os.Getwd() // Get current working directory.
-	if err != nil {
-        fmt.Fprintln(os.Stderr, "Error getting current directory:", err)
-        return
-    }
-	fmt.Println(dir) // Print the current working directory.
-}
-
-// handleCd changes the current working directory.
-func handleCd(args []string) {
-	if len(args) != 1 {
-        fmt.Println("cd: too many arguments")
-        return
-    }
-
-    path := args[0]
-
-    // Check for home directory shortcut (~)
-    if path == "~" {
-        homeDir := os.Getenv("HOME") // Get user's home directory from environment variable.
-        if homeDir == "" {
-            fmt.Println("cd: HOME not set")
-            return
-        }
-        path = homeDir // Use home directory path.
-    }
-
-    // Change to the specified directory.
-    if err := os.Chdir(path); err != nil {
-        fmt.Printf("cd: %s: No such file or directory\n", path)
-    }
-}
-
-// handleType determines how a command would be interpreted and prints the result.
-func handleType(args []string) {
-	if len(args) == 0 {
-        fmt.Println("type: missing argument")
-        return
-    }
-
-    for _, arg := range args {
-        if desc, exists := builtins[arg]; exists {
-            fmt.Printf("%s %s\n", arg, desc)
-            continue
-        }
-		
-        // Check if it's an executable in PATH.
-        path, found := findExecutable(arg)
-        if found {
-            fmt.Printf("%s is %s\n", arg, path)
-        } else {
-            fmt.Printf("%s: not found\n", arg)
-        }
-    }
-}
-
-// findExecutable searches for an executable in the directories listed in PATH.
-func findExecutable(cmd string) (string, bool) {
-    pathEnv := os.Getenv("PATH")
-    directories := strings.Split(pathEnv, ":")
-
-    for _, dir := range directories {
-        fullPath := filepath.Join(dir, cmd)
-        if _, err := os.Stat(fullPath); err == nil {
-            return fullPath, true // Found executable.
-        }
-    }
-    return "", false // Not found.
-}
-
-// executeExternalCommand runs an external command with arguments and captures its output.
-func executeExternalCommand(cmdName string, args []string) error {
-    cmdPath, found := findExecutable(cmdName)
-    if !found {
-        return fmt.Errorf("command not found")
-    }
-
-    cmd := exec.Command(cmdPath, args...) // Create command with path and arguments.
-
-    // Capture output from the command.
-    output, err := cmd.Output()
-    if err != nil {
-        return fmt.Errorf("error executing command: %v", err)
-    }
-
-    fmt.Print(string(output)) // Print the output of the command.
-    return nil
 }
