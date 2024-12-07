32a07c4ae24049279a8127bf4e6e6493916b7fd9
MITRAJANU <mitrajanu.mishra@gmail.com>
2024-12-07 14:21:17 +0530

    FIX:Executing a quoted executable

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -3,168 +3,251 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
-	"path"
-	"strconv"
+	"path/filepath"
+	"slices"
 	"strings"
+	"unicode"
 )
 
-func main() {
-	for {
-		// Prompt for user input
-		fmt.Fprint(os.Stdout, "$ ")
+const (
+	cExit = "exit"
+	cPwd  = "pwd"
+	cCd   = "cd"
+	cEcho = "echo"
+	cType = "type"
+	cCat  = "cat"
+)
 
-		// Wait for user input
-		s, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
+var (
+	cBuiltins = []string{cExit, cPwd, cCd, cEcho, cType}
+)
 
-		s = strings.Trim(s, "\r\n")
+// absoluteBinpath returns the absolute path of a binary in the system PATH.
+func absoluteBinpath(name string) string {
+	paths := strings.Split(os.Getenv("PATH"), ":")
 
-		command, args := parseCommand(s)
-
-		switch command {
-		case "cd":
-			handleCD(args)
-		case "echo":
-			fmt.Println(strings.Join(args, " "))
-		case "exit":
-			handleExit(args)
-		case "pwd":
-			handlePWD()
-		case "type":
-			handleType(args)
-		default:
-			executeCommand(command, args)
+	for _, path := range paths {
+		fullpath := filepath.Join(path, name)
+		if _, err := os.Stat(fullpath); err == nil {
+			return fullpath
 		}
 	}
+
+	return ""
 }
 
-func parseCommand(input string) (string, []string) {
-	var command string
-	var args []string
+// buildString constructs a string from byte slice with specific trimming logic.
+func buildString(s []byte, startIndex, lastIndex, spaceCheckIndex int, indicesToTrim []int) (original, sanitized string) {
+	var output strings.Builder
+	current := startIndex
 
-	if strings.HasPrefix(input, "'") || strings.HasPrefix(input, "\"") {
-		input = strings.Trim(input, "'\"")
+	for _, index := range indicesToTrim {
+		output.Write(s[current:index])
+		current = index + 1
 	}
 
-	commandEnd := strings.IndexAny(input, " ")
-	if commandEnd == -1 {
-		command = input
-	} else {
-		command = input[:commandEnd]
-		argstr := input[commandEnd+1:]
+	if current <= lastIndex {
+		output.Write(s[current:lastIndex])
+	}
 
-		var singleQuote, doubleQuote bool
-		var arg string
+	original = output.String()
+	sanitized = original
 
-		for i := 0; i < len(argstr); i++ {
-			r := rune(argstr[i])
-			switch r {
+	if spaceCheckIndex < len(s) && s[spaceCheckIndex] == ' ' {
+		sanitized += " "
+	}
+
+	return original, sanitized
+}
+
+// parseCommandArg parses command arguments from a byte slice.
+func parseCommandArg(arg []byte) (args []string, stringifiedArgs string) {
+	var stringArgs strings.Builder
+	var startIndex int
+	var currentdelim byte
+	var indicesToTrim []int
+
+	maxIndex := len(arg) - 1
+
+	for i := range arg {
+		nextIndex := i + 1
+		prevIndex := i - 1
+		prevPrevIndex := prevIndex - 1
+
+		switch arg[i] {
+		case '\'':
+			switch currentdelim {
 			case '\'':
-				singleQuote = !singleQuote
+				original, sanitized := buildString(arg, startIndex, i, i+1, nil)
+				stringArgs.WriteString(sanitized)
+				args = append(args, original)
+				startIndex = i + 1
+				currentdelim = 0
+			case 0:
+				startIndex = i + 1
+				currentdelim = '\''
+			}
+			continue
+
+		case '"':
+			switch currentdelim {
 			case '"':
-				doubleQuote = !doubleQuote
-			case ' ':
-				if singleQuote || doubleQuote {
-					arg += string(r)
-				} else if arg != "" {
-					args = append(args, arg)
-					arg = ""
+				if arg[prevIndex] != '\\' || (prevPrevIndex >= 0 && arg[prevPrevIndex] == '\\') {
+					original, sanitized := buildString(arg, startIndex, i, i+1, indicesToTrim)
+					stringArgs.WriteString(sanitized)
+					args = append(args, original)
+					startIndex = i + 1
+					currentdelim = 0
+					indicesToTrim = nil
+					continue
 				}
-			default:
-				arg += string(r)
+			case 0:
+				if prevIndex < 0 || arg[prevIndex] != '\\' || (prevPrevIndex >= 0 && arg[prevPrevIndex] == '\\') {
+					startIndex = i + 1
+					currentdelim = '"'
+					continue
+				}
+			}
+
+		case ' ':
+			if currentdelim == 0 {
+				startIndex = i + 1
+			}
+			continue
+
+		case '\\':
+			if currentdelim == '\'' {
+				continue
+			}
+			noDelimSkip := nextIndex <= maxIndex && currentdelim == 0 && (arg[nextIndex] == '\\' || arg[nextIndex] == '"' || arg[nextIndex] == ' ' || arg[nextIndex] == 'n')
+			quaotationDelimSkip := nextIndex <= maxIndex && currentdelim == '"' && (arg[nextIndex] == '\\' || arg[nextIndex] == '"')
+
+			if (noDelimSkip || quaotationDelimSkip) && (prevIndex < 0 || arg[prevIndex] != '\\') {
+				indicesToTrim = append(indicesToTrim, i)
 			}
 		}
 
-		if arg != "" {
-			args = append(args, arg)
+		if currentdelim == 0 && (nextIndex > maxIndex || arg[nextIndex] == ' ') {
+			original, sanitized := buildString(arg, startIndex, nextIndex, nextIndex, indicesToTrim)
+			stringArgs.WriteString(sanitized)
+			args = append(args, original)
+			startIndex = i + 1
+			indicesToTrim = nil
 		}
 	}
 
-	return command, args
+	return args, stringArgs.String()
 }
 
-func handleCD(args []string) {
-	if len(args) == 0 {
-		fmt.Println("cd: missing argument")
-		return
-	}
-	if args[0] == "~" {
-		args[0] = os.Getenv("HOME")
-	}
-	if err := os.Chdir(args[0]); os.IsNotExist(err) {
-		fmt.Println("cd: " + args[0] + ": No such file or directory")
-	} else if err != nil {
-		log.Fatal(err)
-	}
-}
+// executeCommand executes a command based on its type and arguments.
+func executeCommand(commandType, commandArg string) string {
+	switch commandType {
+	case cExit:
+		os.Exit(0)
 
-func handleExit(args []string) {
-	n := 0
-	if len(args) > 0 {
-		var err error
-		n, err = strconv.Atoi(args[0])
+	case cPwd:
+		dir, err := os.Getwd()
 		if err != nil {
-			log.Fatal(err)
+			panic(fmt.Errorf("unable to obtain current working directory: %w", err))
+		}
+		return dir
+
+	case cCd:
+		if commandArg == "~" {
+			commandArg = os.Getenv("HOME")
+		}
+		if err := os.Chdir(commandArg); err != nil {
+			return fmt.Sprintf("cd: %v: No such file or directory", commandArg)
+		}
+		return ""
+
+	case cEcho:
+		_, output := parseCommandArg([]byte(commandArg))
+		return output
+
+	case cType:
+		if slices.Contains(cBuiltins, commandArg) {
+			return fmt.Sprintf("%v is a shell builtin", commandArg)
+		}
+
+		fullpath := absoluteBinpath(commandArg)
+		if fullpath != "" {
+			return fmt.Sprintf("%v is %v", commandArg, fullpath)
+		}
+		return fmt.Sprintf("%v: not found", commandArg)
+
+	default:
+		_, command := parseCommandArg([]byte(commandType))
+		if fullpath := absoluteBinpath(command); fullpath != "" {
+			args := []string{commandArg}
+			if commandType == cCat {
+				args, _ = parseCommandArg([]byte(commandArg))
+			}
+			cmd := exec.Command(fullpath, args...)
+			output, err := cmd.Output()
+			if err != nil {
+				panic(fmt.Errorf("unable to run external program: %w", err))
+			}
+			return strings.TrimRightFunc(string(output), unicode.IsSpace)
 		}
 	}
-	os.Exit(n)
+
+	return fmt.Sprintf("%v: command not found", commandType)
 }
 
-func handlePWD() {
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(dir)
-}
-
-func handleType(args []string) {
-	if len(args) == 0 {
-		fmt.Println("type: missing argument")
-		return
-	}
-
-	builtin := []string{"cd", "echo", "exit", "pwd", "type"}
-	isBuiltin := false
-
-	for _, cmd := range builtin {
-		if args[0] == cmd {
-			isBuiltin = true
-			break
+// main function to run the shell.
+func main() {
+	for {
+		fmt.Printf("$ ")
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			panic(fmt.Errorf("unable to read input: %w", err))
 		}
-	}
 
-	pathVar := os.Getenv("PATH")
-	var cmdPath string
+		command := input[:len(input)-1]
+		args := make([]string, 2)
 
-	for _, dir := range strings.Split(pathVar, ":") { // Assuming ':' as path separator for Unix-like systems.
-		if _, err := os.Stat(path.Join(dir, args[0])); err == nil {
-			cmdPath = path.Join(dir, args[0])
-			break
+		var delim byte = ' '
+		var stopIndex, continueIndex int
+		var delimpresent bool
+
+		for i := range command {
+			if i == 0 {
+				if command[i] == '\'' || command[i] == '"' {
+					delim = command[i]
+				}
+				continue
+			}
+
+			if command[i] == delim {
+				switch delim {
+				case ' ':
+					stopIndex = i
+					continueIndex = i + 1
+
+				default:
+					stopIndex = i + 1
+					continueIndex = stopIndex + 1
+				}
+
+				delimpresent = true
+				break
+			}
 		}
-	}
 
-	if isBuiltin {
-		fmt.Println(args[0] + " is a shell builtin")
-	} else if cmdPath != "" {
-		fmt.Println(args[0] + " is " + cmdPath)
-	} else {
-		fmt.Println(args[0] + ": not found")
-	}
-}
+		if !delimpresent {
+			stopIndex = len(command)
+			continueIndex = stopIndex
+		}
 
-func executeCommand(command string, args []string) {
-	cmd := exec.Command(command, args...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+		args[0] = command[:stopIndex]
+		args[1] = command[continueIndex:]
 
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+		output := executeCommand(args[0], args[1])
+		if output != "" {
+			fmt.Println(output)
+		}
 	}
 }
